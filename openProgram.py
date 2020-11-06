0bbbb24661110b1dd171cf03e511403234c51ea3
jarrodskahill <jarrodskahill@gmail.com>
2020-11-05 16:19:16 -0800

    now opens website

--- openProgram.py
+++ openProgram.py
@@ -1,8 +1,13 @@
 import os
+import webbrowser
 
 
 def openBrave():
-    os.startfile('brave.exe')
+    url = 'https://oldschool.runescape.wiki/w/Graceful_outfit'
+    webbrowser.register('brave', None, webbrowser.BackgroundBrowser(
+        "C:\Program Files (x86)\BraveSoftware\Brave-Browser\Application\Brave.exe"))
+    # os.startfile('brave.exe')
+    webbrowser.get('brave').open(url)
 
 
 openBrave()
